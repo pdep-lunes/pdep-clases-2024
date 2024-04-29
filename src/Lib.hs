4a8c08b240ae39bedbdbf561a55e9c59d507c991
Julián Berbel Alt <julianberbel@gmail.com>
2024-04-29 11:17:13 -0300

    Agrego la analogia del album de fotos

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,3 +10,16 @@ data Personaje = UnPersonaje {
 
 type PoderBasico = Personaje -> Personaje
 type SuperPoder = String
+
+{-
+Repositorio: "Album de fotos"
+Repositorio remoto: "Copia del album en la nube"
+Repositorio local:  "Copia del album en mi compu"
+
+git status: "Ver el estado de la escena"
+git add <path del archivo>: "Che archivo, ¡metete en la escena para la foto!"
+git commit -m "Un nombre expresivo": "Sacar y nombrar la foto"
+git push origin HEAD: "Subir los cambios del album en mi compu a la nube"
+git pull: "Bajar los cambios del album en la nube a mi compu"
+git log: "Mostrar el listado de fotos"
+-}
